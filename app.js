21cdd72792751e35aa2d37bd02c6aa6ce666ce6b
Jesus Ordosgoitty <jesuodz@gmail.com>
2022-10-15 21:42:36 -0400

    fix cors issues

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-const path = require('path');
 const app = express();
+const cors = require("cors");
 
 // Setting up
 const { APP_PORT } = require('./config');
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(helmet());
